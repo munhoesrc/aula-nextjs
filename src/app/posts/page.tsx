6421373a49841f8221497ee21d14f48488234045
Renan Casarin Munhoes <munhoesrc@gmail.com>
2025-04-10 22:31:21 -0300

    chore: Server x Client component / Server Actions - part 2

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,3 @@
-import { Button } from "@/components/button";
-
 interface PostProps {
   id: number;
   title: string;
@@ -15,13 +13,49 @@ export default async function PostsPage() {
   const response = await fetch("https://dummyjson.com/posts");
   const data: ResponseProps = await response.json();
 
-  console.log(data);
+  async function handleFetchPosts() {
+    "use server";
+
+    console.log("CLICOUUUU");
+  }
+
+  async function handleSeachUsers(formData: FormData) {
+    "use server";
+
+    const userId = formData.get("userId");
+    const response = await fetch(`https://dummyjson.com/posts/user/${userId}`);
+    const data: ResponseProps = await response.json();
+    console.log(data);
+  }
 
   return (
     <div>
       <h1 className="text-4xl font-bold text-center mt-10">Todos os posts</h1>
 
-      <Button />
+      <button
+        onClick={handleFetchPosts}
+        className="bg-blue-500 text-white p-2 rounded-md my-5 mx-auto block cursor-pointer"
+      >
+        Buscar os posts
+      </button>
+
+      <form
+        className="flex flex-col gap-4 mx-auto w-1/2"
+        action={handleSeachUsers}
+      >
+        <input
+          type="text"
+          placeholder="ID do usuário"
+          className="border-2 border-gray-300 p-2 rounded-md w-full mb-4"
+          name="userId"
+        />
+        <button
+          className="bg-blue-500 text-white p-2 rounded-md mb-4 cursor-pointer"
+          type="submit"
+        >
+          Captar usuário
+        </button>
+      </form>
 
       <div className="flex flex-col gap-4 mx-2">
         {data.posts.map((post: any) => (
