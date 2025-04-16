899100ec47111174b21d38cdb159b9adba11a141
Renan Casarin Munhoes <munhoesrc@gmail.com>
2025-04-15 23:59:17 -0300

    chore: Instalação Shadcn UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@ import "./globals.css";
 
 import { Header } from "../components/header";
 import { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Aula Next JS do zero!",
@@ -28,7 +32,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
         <Header />
         {children}
       </body>
