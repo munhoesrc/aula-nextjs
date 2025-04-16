cef5e0e89be41e92c1e048b6093c736b10623646
Renan Casarin Munhoes <munhoesrc@gmail.com>
2025-04-16 00:24:03 -0300

    feat: adicionar botões com shadcn (funcionalidade será implementada na próxima tarefa)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "../../components/ui/button";
+
 export default function Dashboard() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">Dashboard</h1>
+      <div className="flex flex-wrap gap-x-4">
+        <Button className=" text-white p-2 rounded">Voltar</Button>
+        <Button className=" text-white p-2 rounded">Avançar</Button>
+      </div>
       <p className="mt-4 text-lg">Bem-vindo ao painel de controle!</p>
     </main>
   );
